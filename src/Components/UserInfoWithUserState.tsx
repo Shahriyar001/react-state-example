2da3edb2a270bc2aa229d8da6f6f3e9695dd6d95
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-09-24 22:38:51 +0600

    use object as state for a component

--- src/Components/UserInfoWithUserState.tsx
+++ src/Components/UserInfoWithUserState.tsx
@@ -1,22 +1,38 @@
+import { useState } from "react";
+
 const UserInfoWithUserState = () => {
+  const [user, setUser] = useState({ name: "", age: "0", hobbies: [] });
+  console.log(user);
   return (
-    <form>
+    <form className="gap-2">
       <input
-        className="border border-primary"
+        onChange={(e) => setUser({ ...user, name: e.target.value })}
+        className="border border-primary m-10"
         type="text"
         name="name"
         id="name"
         placeholder="name"
       />
       <input
-        className="border border-primary"
+        onChange={(e) => setUser({ ...user, age: e.target.value })}
+        className="border border-primary m-10"
         type="number"
         name="age"
         id="age"
         placeholder="age"
       />
+      {/* <input
+        className="border border-primary m-10"
+        type="number"
+        name="age"
+        id="age"
+        placeholder="age"
+      /> */}
       <input
-        className="border border-primary"
+        onBlur={(e) =>
+          setUser({ ...user, hobbies: [...user.hobbies, e.target.value] })
+        }
+        className="border border-primary m-10"
         type="text"
         name="hobbies"
         id="hobbies"
