ff6d21c4376055e547c20bb71ae2ec902a2be5c4
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-09-26 21:31:55 +0600

    initialize useReducer inside provider component

--- src/context/TodoProvider.tsx
+++ src/context/TodoProvider.tsx
@@ -1,14 +1,42 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useReducer } from "react";
 
-export const TodoContext = createContext(undefined);
+export const TodoContext = createContext<
+  { state: TTodo[]; dispatch: React.Dispatch<TAction> } | undefined
+>(undefined);
+
+type TTodo = {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+};
+
+type TAction = {
+  type: string;
+  payload: TTodo;
+};
+
+const initialState: TTodo[] = [];
+
+const reducer = (currentState: TTodo[], action: TAction) => {
+  switch (action.type) {
+    case "addTodo":
+      return [...currentState, action.payload];
+
+    default:
+      return currentState;
+  }
+};
 
 type TodoProviderProps = {
   children: ReactNode;
 };
 
 const TodoProvider = ({ children }: TodoProviderProps) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const values = {
-    todoTitle: "This is a todo title",
+    state,
+    dispatch,
   };
 
   return <TodoContext.Provider value={values}>{children}</TodoContext.Provider>;
